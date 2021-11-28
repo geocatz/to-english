5224545e6cf583a8e698c09b41cc3e812c1fffc7
ntatko <tatkon@rbtrainers.com>
2021-11-28 15:01:50 -0600

    add support for base58 encoded addresses

--- src/toHuman.js
+++ src/toHuman.js
@@ -2,16 +2,23 @@ const fs = require('fs')
 
 const main = () => {
     const addressFull = process.argv.slice(2)[0]
-
+    
     if (addressFull.slice(0, 2) === "0x") {
-        const address = addressFull.slice(2).toLowerCase().padStart(addressFull.length - 1 + addressFull.slice(2).length % 3, 0)
-
         const dictionary = JSON.parse(fs.readFileSync('dictionary.hex.json'))
+        const address = addressFull.slice(2).toLowerCase().padStart(addressFull.length - 1 + addressFull.slice(2).length % 3, '#')
 
         const chunks = address.match(/.{1,3}/g)
         const words = chunks.map(chunk => dictionary[chunk]).join(' ')
 
-        console.log("chunks", words)
+        console.log("hex " + words)
+    } else {
+        const dictionary = JSON.parse(fs.readFileSync('dictionary.base58.json'))
+        const paddedAddress = addressFull.padStart(addressFull.length + addressFull.length % 2, "#")
+
+        const chunks = paddedAddress.match(/.{1,2}/g)
+        const words = chunks.map(chunk => dictionary[chunk]).join(' ')
+
+        console.log("base58 " + words)
     }
 }
 
--- src/toMachine.js
+++ src/toMachine.js
@@ -3,12 +3,21 @@ const fs = require('fs')
 const main = () => {
     const words = process.argv.slice(2)
 
-    const dictionary = JSON.parse(fs.readFileSync('dictionary.hex.json'))
-    const hexChunks = words.map((word) => Object.keys(dictionary)[Object.values(dictionary).indexOf(word)])
-    hexChunks[0] = hexChunks[0].slice(2)
+    if (words[0].toLowerCase() === "hex") {
+        const dictionary = JSON.parse(fs.readFileSync('dictionary.hex.json'))
+        const hexChunks = words.slice(1).map((word) => Object.keys(dictionary)[Object.values(dictionary).indexOf(word)])
 
-    const string = '0x' + hexChunks.join('')
-    console.log("string", string)
+        const string = '0x' + hexChunks.join('').replace(/#/g, '')
+        console.log("wallet address: " + string)
+    } else if (wrods[0].toLowerCase() === "base58") {
+        const dictionary = JSON.parse(fs.readFileSync('dictionary.base58.json'))
+        const hexChunks = words.slice(1).map((word) => Object.keys(dictionary)[Object.values(dictionary).indexOf(word)])
+
+        const string = hexChunks.join('').replace(/#/g, '')
+        console.log("wallet address: " + string)
+    }
+
+    
 }
 
 main()
